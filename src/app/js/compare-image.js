65c5d9ecd83683a244e44a2d83294567e7ad2fff
Artur SD <arko8919@gmail.com>
2021-06-03 23:33:30 +0100

    Initial dist subtree commit

--- src/app/js/compare-image.js
+++
@@ -1,9 +0,0 @@
-export function compareImage(buttonNumber, imageNumber) {
-    if ((imageNumber[0] === imageNumber[1]) && imageNumber[buttonNumber] === 9) {
-        return imageNumber[buttonNumber] = 0;
-    } else if (imageNumber[0] === imageNumber[1]) {
-        return imageNumber[buttonNumber] + 1;
-    } else {
-        return imageNumber[buttonNumber];
-    }
-}
--- src/app/js/update-array.js
+++
@@ -1,7 +0,0 @@
-export function updateArray(buttonNumber, imageNumber) {
-    if (imageNumber[buttonNumber] === 9) {
-        return imageNumber[buttonNumber] = 0;
-    } else {
-        return imageNumber[buttonNumber] = imageNumber[buttonNumber] + 1;
-    }
-}
