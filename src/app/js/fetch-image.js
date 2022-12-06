557922a29f11d1156e339d03aa8117106e90a925
Artur SD <arko8919@gmail.com>
2022-12-06 08:57:21 +0000

    update

--- src/app/js/fetch-image.js
+++ src/app/js/fetch-image.js
@@ -1,37 +1,40 @@
 export const fetching = function () {
-    const imgRef = document.querySelectorAll('.image-wrapper img');
-    const imgCaption = document.querySelectorAll('section h2');
+  const imgRef = document.querySelectorAll(".image-wrapper img");
+  const imgCaption = document.querySelectorAll("section h2");
 
-    /* URL to fetch data from Instagram API */
-    const fields = 'caption, media_url';
-    const accessToken = 'IGQVJXWUFvWlNNN09MMzRwZAi1NRzhIRllpZAmJvOURQRDkyb29EV01oYjA5MmVIYUVJUFE5OUYwYy1LQlVWSjNpZAi0tRDQ3Y3Bxd2xLd2k3c2k2cVhxX3ZA1TXk0X3dmRXAxMm8xRzdCeW9jUlUzcUt2MAZDZD';
-    const numberOfImages = '&count=10';
-    const url = `https://graph.instagram.com/me/media?fields=${fields}&access_token=${accessToken}${numberOfImages}`;
+  /* URL to fetch data from Instagram API */
+  const fields = "caption, media_url";
+  const accessToken =
+    "IGQVJYaXVCYXduTGkzaEFDejF6d3ZApTTdBZA2Y4N1RDWjl1VE4yTjdZAM3JlUW93S0p0dTVoempXVzZAjcVJGdHdvNEhuYlE0eDN6U0xIbHVrQjBpWkZAfeTJBazJMLXdyMzVIVF9lTUdpeXNTYnhkUzJCZAwZDZD";
+  const numberOfImages = "&count=10";
+  const url = `https://graph.instagram.com/me/media?fields=${fields}&access_token=${accessToken}${numberOfImages}`;
 
-    /* Fetch data from Instagram API and if successful display image number 1 and image number 9 */
-    function fetchImages(url) {
-        return fetch(url)
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error(`HTTP error! status: ${response.status}`);
-                } else {
-                    return response.json();
-                }
-            })
-            .then((json) => {
-                imgRef[0].src = json.data[0].media_url;
-                imgRef[1].src = json.data[9].media_url;
-                imgCaption[0].textContent = json.data[0].caption;
-                imgCaption[1].textContent = json.data[1].caption;
-                return json;
-            })
-            .catch(e => {
-                console.log(`There has been a problem with your fetch operation for resource " ${url}": ${e.message}`);
-            })
-            .finally(() => {
-                console.log(`Fetch attempt finished.`);
-            });
-    }
+  /* Fetch data from Instagram API and if successful display image number 1 and image number 9 */
+  function fetchImages(url) {
+    return fetch(url)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        } else {
+          return response.json();
+        }
+      })
+      .then((json) => {
+        imgRef[0].src = json.data[0].media_url;
+        imgRef[1].src = json.data[9].media_url;
+        imgCaption[0].textContent = json.data[0].caption;
+        imgCaption[1].textContent = json.data[1].caption;
+        return json;
+      })
+      .catch((e) => {
+        console.log(
+          `There has been a problem with your fetch operation for resource " ${url}": ${e.message}`
+        );
+      })
+      .finally(() => {
+        console.log(`Fetch attempt finished.`);
+      });
+  }
 
-    return fetchImages(url);
+  return fetchImages(url);
 };
--- src/index.js
+++ src/index.js
@@ -1,26 +1,32 @@
 /* Import styles */
-import './normalize.css';
-import './style.scss';
+import "./normalize.css";
+import "./style.scss";
 /* Import javascript */
-import {fetching} from './app/js/fetch-image';
-import {changeImage} from './app/js/change-image';
+import { fetching } from "./app/js/fetch-image";
+import { changeImage } from "./app/js/change-image";
 
 /* DOM elements references */
-const buttonRef = document.querySelectorAll('button');
-const imgRef = document.querySelectorAll('.image-wrapper img');
-const imgCaption = document.querySelectorAll('section h2');
+const buttonRef = document.querySelectorAll("button");
+const imgRef = document.querySelectorAll(".image-wrapper img");
+const imgCaption = document.querySelectorAll("section h2");
 
 /* Images with numbers 1 and 10 */
 let imageNumber = [0, 9];
 /* Data from instagram api - media_url and caption */
 let responseObject = {};
 
-fetching().then(json => {
-    responseObject = json;
+fetching().then((json) => {
+  responseObject = json;
 
-    for (let buttonNumber = 0; buttonNumber < buttonRef.length; buttonNumber++) {
-        buttonRef[buttonNumber].addEventListener('click', () => {
-            imageNumber = changeImage(imgRef[buttonNumber], imgCaption[buttonNumber], buttonNumber, imageNumber, responseObject);
-        });
-    }
+  for (let buttonNumber = 0; buttonNumber < buttonRef.length; buttonNumber++) {
+    buttonRef[buttonNumber].addEventListener("click", () => {
+      imageNumber = changeImage(
+        imgRef[buttonNumber],
+        imgCaption[buttonNumber],
+        buttonNumber,
+        imageNumber,
+        responseObject
+      );
+    });
+  }
 });
